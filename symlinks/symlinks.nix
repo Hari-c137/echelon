55bf0e883140a96de3abcc4f5129eb40dd3a0f25
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-12-02 16:15:37 +0530

    recursive symlink config-system & niri rounded-corners:0

--- symlinks/symlinks.nix
+++ symlinks/symlinks.nix
@@ -1,28 +1,14 @@
-{config, ...}:
-# todo: refactor this with loop
 {
-  xdg.configFile."niri" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/niri/";
+  config,
+  lib,
+  ...
+}: let
+  cfgs = ["niri" "doom" "ewwii" "nvim" "mpv"];
+in {
+  xdg.configFile = lib.genAttrs cfgs (name: {
+    source =
+      config.lib.file.mkOutOfStoreSymlink
+      "/home/x137/code/echelon/symlinks/${name}/";
     recursive = true;
-  };
-
-  xdg.configFile."doom" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/doom/";
-    recursive = true;
-  };
-
-  xdg.configFile."ewwii" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/ewwii/";
-    recursive = true;
-  };
-
-  xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/nvim/";
-    recursive = true;
-  };
-
-  xdg.configFile."mpv" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/mpv/";
-    recursive = true;
-  };
+  });
 }
