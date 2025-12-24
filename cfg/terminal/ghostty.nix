a6b864ed37ce577cf84d09d5e8ea406f337486e9
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-12-24 14:44:34 +0530

    ghostty goto_split:next bind

--- cfg/terminal/ghostty.nix
+++ cfg/terminal/ghostty.nix
@@ -9,6 +9,9 @@
       theme = "Gruvbox Material Dark";
       command = "/run/current-system/sw/bin/fish";
       # custom-shader = "~/code/echelon/workers/shaders/cursor_warp.glsl";
+      keybind = [
+        "ctrl+shift+l=goto_split:next"
+      ];
     };
   };
 }
