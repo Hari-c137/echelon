83f0daa01e1aa5a21c4a364f24cf8c2fca5801c9
clearlyopaque <hariprasadaji465@gmail.com>
2026-06-09 12:09:24 +0530

    noctalia-v5 merged to main branch

--- devshells/nim/flake.nix
+++ devshells/nim/flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
   outputs = {
-    self,
     nixpkgs,
   }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-v5 = {
-      url = "github:noctalia-dev/noctalia-shell/v5";
+      url = "github:noctalia-dev/noctalia-shell/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     helium = {
