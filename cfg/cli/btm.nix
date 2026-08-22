8bdec4ad057ea5dec6b1913bfa789fb37a2d634d
clearlyopaque <hariprasadaji465@gmail.com>
2026-08-22 21:58:04 +0530

    fixing some btm deprecation warnings

--- cfg/cli/btm.nix
+++ cfg/cli/btm.nix
@@ -2,12 +2,12 @@
   programs.bottom.enable = true;
   programs.bottom.settings = {
     flags = {
-      process_memory_as_value = true;
-      cpu_left_legend = true;
+      process.default_memory_value = true;
+      cpu.left_legend = true;
       enable_gpu = true;
-      enable_cache_memory = true;
-      process_command = true;
-      regex = true;
+      memory_graph.cache_memory = true;
+      processes.process_command = true;
+      processes.regex = true;
     };
   };
 }
